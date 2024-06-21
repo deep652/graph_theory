c6e387af98db47b9b1d1be0a5d8d865b635f551f
polyglot-vikash <viku.nitdgpe@gmail.com>
2024-06-21 07:02:50 +0530

    finished the printing of all the paths

--- README.md
+++ README.md
@@ -13,7 +13,28 @@ Rat in  a maze problem
 
         In both the versions, the way we backtrack is a bit different.
 
-3. 
+3. important points
+   1. while dealing with recursion, it is important to save the result somewhere which doesn't get lost while you backtrack
+   2. in the below line of code it is important to note two things and understand it
+   3. for(int i=0; i<4; i++)
+        {
+            int newx = x + rowDir[i];
+            int newy = y + colDir[i];
+            //cout<<x<<","<<y<<endl;
+            if(isSafe(newx,newy))
+            {
+                result.push_back(make_pair(newx,newy));
+                dfs( desX, desY, newx, newy);
+                result.pop_back();
+            }
+        }
+        visited[x][y] = 0;
+
+        1. at line 28, if reach at apoint where you can't move forward, you need to backtrack, that will be handled by the call stack (which is the root of dfs) but before we do, the path/result need to be updated, i.e. the indexs we already pushed need to poped out, because followinf that path we didn't reach to our destination
+        2.  why we have marked visited[x][y] = 0 at the outside of the loop
+        because understand the recursion
+        when you called dfs on 0, 0 you will try to move all direction from 0. the loop is for that movement. now while doing so, you are also calling the recursion on the dfs, this means a new copy of the method will get called with new x, y
+        so when we reach a point when we can no longer move in any direction, we need to go back, that is when we come out of the loop (movement loop). and that is where we will want the x, y to be  marked as unvisited. 
      
 
 
---
+++ mazeTraversal.cpp
@@ -0,0 +1,136 @@
+#include<iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+class MazeTraversal
+{
+    private:
+    vector<vector<int> > maze;;
+    vector<vector<int> > visited;
+    int row = 4;
+    int col = 4;
+    int rowDir[4] =  {1, -1, 0, 0}; //right , left, 0, 0
+    int colDir[4] = {0, 0, 1, -1}; //0, 0, up, down
+    
+    vector<vector<pair<int, int>>> allPaths;
+    vector<pair<int,int> > result;
+    public:
+    MazeTraversal():visited(4, vector<int>(4,0)){}
+    MazeTraversal(vector<vector<int> >& grid): visited(4, vector<int>(4, 0)), maze(grid){}
+    bool isSafe(int x, int y)
+    {
+        if((x>=0 && x < row) && (y>=0 && y < col) && maze[x][y] == 1 && visited[x][y] == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    void dfs(int desX, int desY, int x, int y)
+    {
+        if(x == desX && y == desY)
+        {
+            allPaths.push_back(result);
+            return;
+        }
+        visited[x][y] = 1;
+        //check from x all row direction and from y all col direction
+        for(int i=0; i<4; i++)
+        {
+            int newx = x + rowDir[i];
+            int newy = y + colDir[i];
+            //cout<<x<<","<<y<<endl;
+            if(isSafe(newx,newy))
+            {
+                result.push_back(make_pair(newx,newy));
+                dfs( desX, desY, newx, newy);
+                result.pop_back();
+            }
+        }
+        visited[x][y] = 0;
+    }
+
+    void print()
+    {
+        for (const auto& p : allPaths)
+        {
+            for (const auto& cell : p) 
+            {
+                cout << "(" << cell.first << ", " << cell.second << ") ";
+            }
+            cout << endl;
+        }
+        
+    }
+};
+
+int main()
+{
+    vector<vector<int> > grid = { 
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 1}
+        };
+    MazeTraversal obj(grid);
+
+    obj.dfs(3,3,0,0);
+    obj.print();
+    return 0;
+}
+
+// 1	0
+// 1	1
+// 1	2
+// 2	2
+// 3	2
+// 3	3
+
+// 3	1
+// 3	0
+// 2	3
+// 3	3
+
+// 1	3
+// 1	3
+// 2	3
+// 3	3
+
+// 2	2
+// 3	2
+// 3	3
+// 3	1
+// 3	0
+
+/**
+ * for the below maze
+
+//  { 
+//         {1, 0, 0, 0},
+//         {1, 1, 1, 1},
+//         {0, 0, 1, 1},
+//         {1, 1, 1, 1}
+// };
+
+// (1, 0) (1, 1) (1, 2) (2, 2) (3, 2) (3, 3) 
+// (1, 0) (1, 1) (1, 2) (2, 2) (2, 3) (3, 3) 
+// (1, 0) (1, 1) (1, 2) (1, 3) (2, 3) (3, 3) 
+// (1, 0) (1, 1) (1, 2) (1, 3) (2, 3) (2, 2) (3, 2) (3, 3) 
+
+**/
+
+/**
+ * for another input
+ *   
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 1}
+        };
+ * 
+ * 
+
+(1, 0) (1, 1) (2, 1) (3, 1) (3, 2) (3, 3) 
+ * **/
